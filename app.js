e0924c92a212e3f810d61d39dfc8b1b187914550
JCodrescu <jcodresc@calpoly.edu>
2022-04-20 13:32:35 -0700

    same as before

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+const port = process.env.PORT || 5000
 
 app.use(cors())
-const port = 3001;
+
+app.get('/', (req, res) => {
+   res.json({'yes': 'wooooo'});
+});
 
 app.get('/gameWinner', (req, res) => {
     res.json({'winner': (Math.floor(Math.random() * 2) === 0 ? 'Heads' : 'Tails')});
